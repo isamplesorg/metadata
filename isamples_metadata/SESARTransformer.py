424871f730a3c97dcb9f36922cdb01fc06a48283
Danny Mandel <dmandel@apple.com>
2021-05-25 11:40:34 -0700

    Checkpoint

--- isamples_metadata/SESARTransformer.py
+++ isamples_metadata/SESARTransformer.py
@@ -22,17 +22,97 @@ class SESARTransformer(Transformer):
     def sample_label(self, source_record: typing.Dict) -> typing.AnyStr:
         return source_record['description']['sampleName']
 
-    def description_string(self, source_record: typing.Dict) -> typing.AnyStr:
+    def sample_description(self, source_record: typing.Dict) -> typing.AnyStr:
+        # TODO: implement
         return ""
 
-    def has_context_category(self, source_record: typing.Dict) -> typing.List:
+    def has_context_categories(self, source_record: typing.Dict) -> typing.List:
         return ['Subsurface fluid reservoir']
 
-    def has_material_category(self, source_record: typing.Dict) -> typing.List:
+    def has_material_categories(self, source_record: typing.Dict) -> typing.List:
         return ['Gaseous material']
 
-    def has_specimen_category(self, source_record: typing.Dict) -> typing.List:
+    def has_specimen_categories(self, source_record: typing.Dict) -> typing.List:
         return ['Container with fluid']
 
     def keywords(self, source_record: typing.Dict) -> typing.List:
         return [source_record['description']['sampleType']]
+
+    def sample_registrant(self, source_record: typing.Dict) -> typing.AnyStr:
+        contributors = source_record['description']['contributors']
+        registrants = list(filter(lambda contributor_dict: contributor_dict['roleName'] == 'Sample Registrant', contributors))
+        if len(registrants) > 0:
+            return registrants[0]['contributor'][0]['name']
+        else:
+            return ""
+
+    def sample_sampling_purpose(self, source_record: typing.Dict) -> typing.AnyStr:
+        # TODO: implement
+        return ""
+
+    def produced_by_label(self, source_record: typing.Dict) -> typing.AnyStr:
+        return source_record['description']['collectionMethod']
+
+    def produced_by_description(self, source_record: typing.Dict) -> typing.AnyStr:
+        description_str = ''
+        description_dict = source_record['description']
+        if description_dict != None:
+            supplement_metadata = description_dict['supplementMetadata']
+            if supplement_metadata != None:
+                if 'cruiseFieldPrgrm' in supplement_metadata:
+                    description_str += 'cruiseFieldPrgrm:{0}. '.format(supplement_metadata['cruiseFieldPrgrm'])
+                if 'launchPlatformName' in supplement_metadata:
+                    description_str += 'launchPlatformName:{0}. '.format(supplement_metadata['launchPlatformName'])
+
+            if 'collectionMethod' in description_dict:
+                description_str += '{0}. '.format(description_dict['collectionMethod'])
+            if 'description' in description_dict:
+                description_str += '{0}. '.format(description_dict['description'])
+
+            if supplement_metadata != None:
+                if 'launchTypeName' in supplement_metadata:
+                    description_str += 'launch type:{0}, '.format(supplement_metadata['launchTypeName'])
+                if 'navigationType' in supplement_metadata:
+                    description_str += 'navigation type:{0}'.format(supplement_metadata['navigationType'])
+        return description_str
+
+    def produced_by_feature_of_interest(self, source_record: typing.Dict) -> typing.AnyStr:
+        description_dict = source_record['description']
+        if description_dict != None:
+            supplement_metadata = description_dict['supplementMetadata']
+            if supplement_metadata != None and 'primaryLocationType' in supplement_metadata:
+                return supplement_metadata['primaryLocationType']
+        return ""
+
+    def produced_by_responsibilities(self, source_record: typing.Dict) -> typing.List:
+        # TODO: implement
+        return []
+
+    def produced_by_result_time(self, source_record: typing.Dict) -> typing.AnyStr:
+        # TODO: implement
+        return ""
+
+    def sampling_site_description(self, source_record: typing.Dict) -> typing.AnyStr:
+        # TODO: implement
+        return ""
+
+    def sampling_site_label(self, source_record: typing.Dict) -> typing.AnyStr:
+        # TODO: implement
+        return ""
+
+    def sampling_site_elevation(self, source_record: typing.Dict) -> typing.AnyStr:
+        # TODO: implement
+        return ""
+
+    def sampling_site_latitude(self, source_record: typing.Dict) -> typing.SupportsFloat:
+        # TODO: implement
+        return 0.0
+
+    def sampling_site_longitude(self, source_record: typing.Dict) -> typing.SupportsFloat:
+        # TODO: implement
+        return 0.0
+
+    def sampling_site_place_names(self, source_record: typing.Dict) -> typing.List:
+        # TODO: implement
+        return []
+
--- isamples_metadata/Transformer.py
+++ isamples_metadata/Transformer.py
@@ -18,11 +18,31 @@ class Transformer(ABC):
                               '@id': 'https://data.isamples.org/digitalsample/{0}/{1}'.format(self.sample_identifier_scheme(source_record), self.sample_identifier_value(source_record)),
                               'label': self.sample_label(source_record),
                               'sampleidentifier': '{0}:{1}'.format(self.sample_identifier_scheme(source_record), self.sample_identifier_value(source_record)),
-                              'description': self.description_string(source_record),
-                              'hasContextCategory': self.has_context_category(source_record),
-                              'hasMaterialCategory': self.has_material_category(source_record),
-                              'hasSpecimenCategory': self.has_specimen_category(source_record),
-                              'keywords': self.keywords(source_record)}
+                              'description': self.sample_description(source_record),
+                              'hasContextCategory': self.has_context_categories(source_record),
+                              'hasMaterialCategory': self.has_material_categories(source_record),
+                              'hasSpecimenCategory': self.has_specimen_categories(source_record),
+                              'keywords': self.keywords(source_record),
+                              'producedBy': {
+                                  'label': self.produced_by_label(source_record),
+                                  'description': self.produced_by_description(source_record),
+                                  'hasFeatureOfInterest': self.produced_by_feature_of_interest(source_record),
+                                  'responsibility': self.produced_by_responsibilities(source_record),
+                                  'resultTime': self.produced_by_result_time(source_record),
+                                  'samplingSite': {
+                                      'description': self.sampling_site_description(source_record),
+                                      'label': self.sampling_site_label(source_record),
+                                      'location': {
+                                          'elevation': self.sampling_site_elevation(source_record),
+                                          'latitude': self.sampling_site_latitude(source_record),
+                                          'longitude': self.sampling_site_longitude(source_record)
+                                      },
+                                      'placeName': self.sampling_site_place_names(source_record)
+                                  }
+                              },
+                              'registrant': self.sample_registrant(source_record),
+                              'samplingPurpose': self.sample_sampling_purpose(source_record)
+                              }
         return transformed_record
 
     @abstractmethod
@@ -41,26 +61,91 @@ class Transformer(ABC):
         pass
 
     @abstractmethod
-    def description_string(self, source_record: typing.Dict) -> typing.AnyStr:
+    def sample_description(self, source_record: typing.Dict) -> typing.AnyStr:
         """A textual description of the sample in source_record"""
         pass
 
     @abstractmethod
-    def has_context_category(self, source_record: typing.Dict) -> typing.List:
+    def sample_registrant(self, source_record: typing.Dict) -> typing.AnyStr:
+        """The name of the sample registrant in source_record"""
+        pass
+
+    @abstractmethod
+    def sample_sampling_purpose(self, source_record: typing.Dict) -> typing.AnyStr:
+        """The samplingPurpose of the sample registrant in source_record"""
+        pass
+
+    @abstractmethod
+    def has_context_categories(self, source_record: typing.Dict) -> typing.List:
         """Map from the source record into an iSamples context category"""
         pass
 
     @abstractmethod
-    def has_material_category(self, source_record: typing.Dict) -> typing.List:
+    def has_material_categories(self, source_record: typing.Dict) -> typing.List:
         """Map from the source record into an iSamples material category"""
         pass
 
     @abstractmethod
-    def has_specimen_category(self, source_record: typing.Dict) -> typing.List:
+    def has_specimen_categories(self, source_record: typing.Dict) -> typing.List:
         """Map from the source record into an iSamples specimen category"""
         pass
 
     @abstractmethod
     def keywords(self, source_record: typing.Dict) -> typing.List:
         """The keywords for the sample in source record"""
+        pass
+
+    @abstractmethod
+    def produced_by_label(self, source_record: typing.Dict) -> typing.AnyStr:
+        """The label for the producedBy dictionary"""
+        pass
+
+    @abstractmethod
+    def produced_by_description(self, source_record: typing.Dict) -> typing.AnyStr:
+        """The description for the producedBy dictionary"""
+        pass
+
+    @abstractmethod
+    def produced_by_feature_of_interest(self, source_record: typing.Dict) -> typing.AnyStr:
+        """The feature of interest for the producedBy dictionary"""
+        pass
+
+    @abstractmethod
+    def produced_by_responsibilities(self, source_record: typing.Dict) -> typing.List:
+        """The responsibility list for the producedBy dictionary"""
+        pass
+
+    @abstractmethod
+    def produced_by_result_time(self, source_record: typing.Dict) -> typing.AnyStr:
+        """The result time for the producedBy dictionary"""
+        pass
+
+    @abstractmethod
+    def sampling_site_description(self, source_record: typing.Dict) -> typing.AnyStr:
+        """The sampling site description"""
+        pass
+
+    @abstractmethod
+    def sampling_site_label(self, source_record: typing.Dict) -> typing.AnyStr:
+        """The sampling site label"""
+        pass
+
+    @abstractmethod
+    def sampling_site_elevation(self, source_record: typing.Dict) -> typing.AnyStr:
+        """The sampling site elevation"""
+        pass
+
+    @abstractmethod
+    def sampling_site_latitude(self, source_record: typing.Dict) -> typing.SupportsFloat:
+        """The sampling site latitude"""
+        pass
+
+    @abstractmethod
+    def sampling_site_longitude(self, source_record: typing.Dict) -> typing.SupportsFloat:
+        """The sampling site longitude"""
+        pass
+
+    @abstractmethod
+    def sampling_site_place_names(self, source_record: typing.Dict) -> typing.List:
+        """The sampling site longitude"""
         pass
