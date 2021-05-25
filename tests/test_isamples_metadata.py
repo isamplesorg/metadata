f789ea24f84c4da8d92d05108674381fd2e23571
Danny Mandel <dmandel@apple.com>
2021-05-25 14:47:15 -0700

    Refactor to make the initial record an instance variable

--- tests/test_isamples_metadata.py
+++ tests/test_isamples_metadata.py
@@ -10,10 +10,11 @@ def test_version():
     print("Test passed")
 
 def test_dicts_equal():
-    transformer = SESARTransformer()
+
     with open('../examples/SESAR/raw/EOI00002Hjson-ld.json') as SESAR_source_file:
         SESAR_record = json.load(SESAR_source_file)
-        transformed_to_iSamples_record = transformer.transform(SESAR_record)
+        transformer = SESARTransformer(SESAR_record)
+        transformed_to_iSamples_record = transformer.transform()
         with open('../examples/SESAR/test/iSamplesEOI00002HBasic.json') as iSamples_file:
             iSamples_record = json.load(iSamples_file)
             assert iSamples_record == transformed_to_iSamples_record
