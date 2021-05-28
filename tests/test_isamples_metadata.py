a5682eb19029c0564ede4ffb17d6e18836d6e42e
Danny Mandel <dmandel@apple.com>
2021-05-28 13:45:05 -0700

    Make the formatting comply with Python recommendations

--- tests/test_isamples_metadata.py
+++ tests/test_isamples_metadata.py
@@ -9,12 +9,13 @@ SESAR_test_values = [
     ('../examples/SESAR/raw/IEEJR000Mjson-ld.json', '../examples/SESAR/test/iSamplesIEEJR000MBasic.json')
 ]
 
-@pytest.mark.parametrize("SESAR_source_path,iSamples_path", SESAR_test_values)
-def test_dicts_equal(SESAR_source_path, iSamples_path):
-    with open(SESAR_source_path) as SESAR_source_file:
-        SESAR_record = json.load(SESAR_source_file)
-        transformer = SESARTransformer(SESAR_record)
-        transformed_to_iSamples_record = transformer.transform()
-        with open(iSamples_path) as iSamples_file:
-            iSamples_record = json.load(iSamples_file)
-            assert transformed_to_iSamples_record == iSamples_record
+
+@pytest.mark.parametrize("sesar_source_path,isamples_path", SESAR_test_values)
+def test_dicts_equal(sesar_source_path, isamples_path):
+    with open(sesar_source_path) as sesar_source_file:
+        sesar_record = json.load(sesar_source_file)
+        transformer = SESARTransformer(sesar_record)
+        transformed_to_isamples_record = transformer.transform()
+        with open(isamples_path) as isamples_file:
+            isamples_record = json.load(isamples_file)
+            assert transformed_to_isamples_record == isamples_record
