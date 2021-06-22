3106cd73ad1704b3b089bc4b6fd6cf975391f812
Danny Mandel <danny.mandel@gmail.com>
2021-06-22 16:12:03 -0700

    Write some unit tests for this

---
+++ tests/test_elevation_values.py
@@ -0,0 +1,16 @@
+import pytest
+
+from isamples_metadata.SESARTransformer import SESARTransformer
+
+@pytest.fixture
+def sesar_transformer():
+    return SESARTransformer({})
+
+def test_elevation_in_meters(sesar_transformer):
+    elevation_str = sesar_transformer.elevation_str(12345, "meters")
+    assert elevation_str == "12345 m"
+
+
+def test_elevation_in_feet(sesar_transformer):
+    elevation_str = sesar_transformer.elevation_str(3.28084, "feet")
+    assert elevation_str == "1.0 m"
