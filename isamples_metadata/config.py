93f8744518d7b1be34a2f4be18f3fab6073b8ab0
Danny Mandel <danny.mandel@gmail.com>
2021-11-03 12:00:33 -0700

    Delete the python code out of the metadata repo (#84)

--- isamples_metadata/config.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseSettings
-
-
-class Settings(BaseSettings):
-    # The absolute path to the FastText model
-    fasttext_model_path: str = "UNSET"
-
-    class Config:
-        env_file = "isamples_metadata.env"
